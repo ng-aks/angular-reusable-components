deba05e13b7631924e2b5d5f1453872fb1bff6bc
Dell <aksharma.dixit@gmail.com>
2024-12-27 21:10:08 +0530

    update signals input output apis for component interaction

--- projects/ng-aks-forms/src/lib/ng-aks-forms.component.ts
+++ projects/ng-aks-forms/src/lib/ng-aks-forms.component.ts
@@ -1,22 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { NgAksFormsConfigModel } from '../public-api';
 import { FormComponent } from "./form/form.component";
-import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'ng-aks-forms',
   imports: [FormComponent],
   template: `
-    <lib-form [formConfig]="formConfig" (onSubmitForm)="getFormGroup($event)"/>
+    <lib-form [formConfig]="formConfig()" (onSubmitForm)="getFormGroup($event)"/>
     <ng-content/>
   `,
   styles: ``
 })
 export class NgAksFormsComponent {
-  @Input() formConfig!: NgAksFormsConfigModel;
-  @Output() onGetForm:EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
-  @Input() isSubmit:boolean = false;
-
+  readonly formConfig = input.required<NgAksFormsConfigModel>();
+  onGetForm = output<FormGroup>();
 
   getFormGroup(e:FormGroup){
     this.onGetForm.emit(e);
